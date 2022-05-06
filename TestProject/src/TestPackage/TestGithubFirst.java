c610b0cf6b0991fbc5831d7c796a50e483f55431
ericgu0619 <ericgu0619@gmail.com>
2022-05-06 21:53:26 +0800

    commit2

---
+++ TestProject/src/TestPackage/TestGithubFirst.java
@@ -0,0 +1,10 @@
+package TestPackage;
+
+public class TestGithubFirst {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello github!!");
+	}
+
+}
